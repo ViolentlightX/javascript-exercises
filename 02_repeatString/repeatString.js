2ab431941b515ffd3418fe12ba8432cd43afdc81
Jason Stewart <35661665+ViolentlightX@users.noreply.github.com>
2022-11-12 09:55:58 -0700

    Assignment complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let compString = '';
+    for (i = 0;i < num;i++) {
+        compString += string;
+    }
+    return compString;
 };
 
 // Do not edit below this line
