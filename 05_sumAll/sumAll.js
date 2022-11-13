a80294c9dba599040cd4d1ec58e2b83e71a16e8a
Jason Stewart <35661665+ViolentlightX@users.noreply.github.com>
2022-11-13 13:02:57 -0700

    Passed first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sumNum = 0;
+    for(i = start; i <= end; i++) {
+        sumNum += i;
+    }
+    return sumNum;
 };
 
 // Do not edit below this line
