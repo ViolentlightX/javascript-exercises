ca1e6e1a5804e99fcd27506e2b6307f1f4d3b3e0
Jason Stewart <35661665+ViolentlightX@users.noreply.github.com>
2022-11-13 13:46:59 -0700

    First test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const round = (number) => {
+  return Math.round(number * 10) / 10;
+}
+const ftoc = function(fahrenheit) {
+  return round((fahrenheit - 32) * (5 /9));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return round(celsius * (9 / 5) + 32);
 };
 
 // Do not edit below this line
